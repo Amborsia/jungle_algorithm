38b31ea7693c36c394cfae454155a48788d7bc5d
bishoe01 <bishoe01@gmail.com>
2024-04-20 16:49:35 +0900

    [SOLVE #3] 올림픽 8979

--- week03/3_8979.py
+++ week03/3_8979.py
@@ -1,22 +1,16 @@
 import sys
+
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 
-arr.sort(key=lambda x: (x[1], x[2], x[3]))
+arr.sort(key=lambda x: (x[1], x[2], x[3]), reverse=True)
 
-
-index_of_target = [arr[i][0] for i in range(n)].index(k)
-tmp = []
+# print('arr: ', arr)
+index_target = [arr[i][0] for i in range(n)].index(k)
 for i in range(n):
-    tmp.append(arr[i][0])
-
-index_of_target = tmp.index(k)
-
-for i in range(n):
-    if (arr[index_of_target][1:] == arr[i][1:]):  # 먼저나오면 =그대로등수고  뒤에 나와도 괜찮네 같이 출력되니까
+    if (arr[index_target][1:] == arr[i][1:]):
         print(i+1)
         break
