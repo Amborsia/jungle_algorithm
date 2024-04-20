38c05cd9a73a6de392b7de28746561de2b68d115
hyeong1 <mi75625@gmail.com>
2024-04-20 11:34:30 +0900

    8979 올림픽 45점

---
+++ week02/hyeong1/8979_올림픽.py
@@ -0,0 +1,15 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+info = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+info.sort(key=lambda x: (x[1], x[2], x[3]), reverse=True)
+for i in range(N):
+    if info[i][0] == K:
+        if info[i][1:] == info[i-1][1:]:
+            print(i)
+            break
+        else:
+            print(i+1)
+            break
+print(info)
