8db0597f2bf064fcf3744ca3de5e99fd3e924727
bishoe01 <bishoe01@gmail.com>
2024-04-24 14:47:53 +0900

    4번 solve 개선

--- week04/4_1755.py
+++ week04/4_1755.py
@@ -3,17 +3,14 @@ m, n = map(int, input().split())
 dic = {'1': 'one', '2': 'two', '3': 'three', '4': 'four', '5': 'five',
        '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine', '0': 'zero'}
 
-arr = []
+arr = [i for i in range(m, n+1)]  # m 부터 n까지 담아주기
+answer = []
+for element in arr:
+    tmp = ''.join(dic[k] for k in str(element))
+    answer.append((element, tmp))
+answer.sort(key=lambda x: x[1])
 
-for i in range(m, n+1):
-    a = ' '.join([dic[j] for j in str(i)])
-    # REVIEW - join으로 묶어주기
-    arr.append([i, a])
-
-
-arr.sort(key=lambda x: x[1])
-
-for i in range(len(arr)):
+for i in range(len(answer)):
     if (i % 10 == 0 and i != 0):
-        print('\n')
-    print(arr[i][0], end=' ')
+        print()
+    print(answer[i][0], end=' ')
